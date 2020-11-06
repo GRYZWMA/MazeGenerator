3b207b02df7d3b43f825bcc483fc652c418ba5f0
Michael Gryzwa <magryzwa@gmail.com>
2020-11-05 20:42:33 -0600

    Made draw_cell function to draw all walls needed around a cell

--- cell.py
+++ cell.py
@@ -1,9 +1,16 @@
 # cell class
 class Cell:
 
+    walls = [True, True, True, True]
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def print_cell(self):
         print("Cell is:(" + str(self.x) + "," + str(self.y) + ")")
+
+    def print_walls(self):
+        print(" Walls is:(" + str(self.walls[0]) + "," + str(self.walls[1]) + "," + str(self.walls[2]) + "," + str(
+            self.walls[3]) + ")")
+
